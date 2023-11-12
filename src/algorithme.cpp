fc0a691193df5255effc57fa69389e6d93c4c8ac
alexlechass <leca1403@usherbrooke.ca>
2023-11-11 21:03:59 -0500

    deuxieme joueur
    il reste a mettre certaines fonction comme suiveur de ligne

--- src/algorithme.cpp
+++ src/algorithme.cpp
@@ -1,14 +1,16 @@
 #include <Arduino.h>
 #include <libRobus.h>
 #include "algorithme.h"
-
+int joueur1;//niveau premier joueur
+int joueur2;//niveau deuxieme joueur
 float vitesse;
-int mode;
+int mode;//un ou deux joueur
 int niveau;
 int i;
 float currentMillis;
 long startTime;
 long duration = 1000;
+int tour_joueur=1;
 void algoInit()
 {
     
@@ -78,18 +80,72 @@ void algo(){
 
 
         case MULTIJOUEUR:
+        niveau=joueur1;
     switch (niveau){
         case 1:
+        for(i=0; i<5;i++){
+            //suiveurligne(vitesse);
+            MOTOR_SetSpeed(LEFT,vitesse);
+            MOTOR_SetSpeed(RIGHT,vitesse);
+            if(1){//detection ligne = 1pour live ou bloc
+            //suiveurligne(0);
+            MOTOR_SetSpeed(LEFT,0);
+            MOTOR_SetSpeed(RIGHT,0);
+            delay(TEMPS);
+            }
+        }
+        if(tour_joueur==2){
+            mode=STOP;
+        }
+        else{
+            tour_joueur++;
+            niveau=joueur2;
+        }
             break;
         
 
 
-        case 2:
+        case 2: i=0;
+        while(i<5){
+            //suiveurligne(vitesse);
+            MOTOR_SetSpeed(LEFT,vitesse);
+            MOTOR_SetSpeed(RIGHT,vitesse);
+            if (1){//detectection incémentation
+                i++;
+            }
+        }
+       if(tour_joueur==2){
+            mode=STOP;
+        }
+        else{
+            tour_joueur++;
+            niveau=joueur2;
+        }
+            break;
+
+
             break;
 
 
 
         case 3:
+        i=0;
+        while(i<5){
+            //suiveurligne(vitesse);
+            vitesse_random(VITESSEMIN,VITESSEMAX);
+            MOTOR_SetSpeed(LEFT,vitesse);
+            MOTOR_SetSpeed(RIGHT,vitesse);
+            if (1){//detectection incémentation
+                i++;
+            }
+        }
+        if(tour_joueur==2){
+            mode=STOP;
+        }
+        else{
+            tour_joueur++;
+            niveau=joueur2;
+        }
         break;
         }
     break;
